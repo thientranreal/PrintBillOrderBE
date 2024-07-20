3bacf8dd10c523454aa101c41efadd40c1a939e1
nluat01995 <luat.nguyenvan@kingfoodmart.net>
2024-07-20 15:58:21 +0700

    fix bug valid

--- schema/loginSchema.js
+++ schema/loginSchema.js
@@ -8,7 +8,7 @@ module.exports.schemaLogin = [
 module.exports.registerSchema = [
     body('email').isEmail().withMessage('Email is not valid'),
     body('password')
-        .exists({ checkFalsy: true }).withMessage('You must type a password'),
+        .exists({ checkFalsy: true }).withMessage('You must type a password').isLength({ min: 5 }),
     body('confirmedPassword')
         .exists({ checkFalsy: true }).withMessage('You must type a confirmation password')
         .custom((value, { req }) => value === req.body.password).withMessage("The passwords do not match")
